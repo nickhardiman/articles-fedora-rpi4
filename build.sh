cbf7933ae1b716ff0fee4b6ea56e7210b45c392c
Nicholas Hardiman <nhardima@redhat.com>
2021-09-21 23:41:49 +0100

    build fix, nav reorder

--- build.sh
+++ build.sh
@@ -3,27 +3,19 @@
 env | sort
 pwd
 ls -la
+#
 # install Antora
-export NODE_VERSION=12
 npm i -g @antora/cli @antora/site-generator-default
+#
 # install lunr search
 npm i -g antora-lunr
-#
 # where are the node modules
-node --version
-# fixed path, will break on upgrade
-NODE_VERSION=v12.18.0
-NODE_MODULES=/opt/buildhome/.nvm/versions/node/$NODE_VERSION/lib/node_modules
-# with find
-# nope, stacks of matches 
-#NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
-# with nvm (node version manager) 
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-#NVM_VERSION=$(nvm version)
-#NODE_MODULES=/opt/buildhome/.nvm/versions/node/$NVM_VERSION/lib/node_modules
-#echo $NODE_MODULES
-#
+# These env vars already exist
+#NODE_VERSION=$(node --version)
+#HOME=/opt/buildhome
+NODE_MODULES=$HOME/.nvm/versions/node/$NODE_VERSION/lib/node_modules
 cp -r $NODE_MODULES/antora-lunr/supplemental_ui .
 cp ./generate-site.js $NODE_MODULES/@antora/site-generator-default/lib/
+#
 # transform asciidoctor to HTML
 DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr antora antora-playbook.yml
