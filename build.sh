4fdd03c8b33dc5430e0344f12f17ddb034bb3d8f
Nicholas Hardiman <nhardima@redhat.com>
2021-09-21 22:47:44 +0100

    wrong path

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 npm i -g @antora/cli @antora/site-generator-default
 # install lunr search
 npm i -g antora-lunr
-cp -r /opt/buildhome/.nvm/versions/node/v12.18.0/lib/node_modules/antora-lunr/supplemental_ui .
-cp ./generate-site.js /opt/buildhome/.nvm/versions/node/v12.18.0/lib/node_modules/@antora/site-generator-default/lib/
+# something like /opt/buildhome/.nvm/versions/node/v12.18.0/lib/node_modules
+NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
+echo $NODE_MODULES
+cp -r $NODE_MODULES/antora-lunr/supplemental_ui .
+cp ./generate-site.js $NODE_MODULES/@antora/site-generator-default/lib/
 # transform asciidoctor to HTML
 DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr antora antora-playbook.yml
