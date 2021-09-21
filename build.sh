6bab2aec18b7b1f467ebd2722cdb80ff6de0bf88
Nicholas Hardiman <nhardima@redhat.com>
2021-09-21 22:53:44 +0100

    node_modules fix

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ npm i -g @antora/cli @antora/site-generator-default
 # install lunr search
 npm i -g antora-lunr
 # something like /opt/buildhome/.nvm/versions/node/v12.18.0/lib/node_modules
-NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
+#NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
+NVM_VERSION=$(nvm version)
+NODE_MODULES=/opt/buildhome/.nvm/versions/node/$NVM_VERSION/lib/node_modules
 echo $NODE_MODULES
 cp -r $NODE_MODULES/antora-lunr/supplemental_ui .
 cp ./generate-site.js $NODE_MODULES/@antora/site-generator-default/lib/
