d26ebb7b696b0b411896fab9a55efb054d1ea255
Nicholas Hardiman <nhardima@redhat.com>
2021-09-21 22:58:07 +0100

    try find

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 npm i -g @antora/cli @antora/site-generator-default
 # install lunr search
 npm i -g antora-lunr
-# something like /opt/buildhome/.nvm/versions/node/v12.18.0/lib/node_modules
-#NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
-NVM_VERSION=$(nvm version)
-NODE_MODULES=/opt/buildhome/.nvm/versions/node/$NVM_VERSION/lib/node_modules
+# find the node modules
+# with find
+NODE_MODULES=$(find /opt/buildhome/.nvm -name  node_modules -type d -prune)
+# with nvm (node version manager) 
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+#NVM_VERSION=$(nvm version)
+#NODE_MODULES=/opt/buildhome/.nvm/versions/node/$NVM_VERSION/lib/node_modules
 echo $NODE_MODULES
 cp -r $NODE_MODULES/antora-lunr/supplemental_ui .
 cp ./generate-site.js $NODE_MODULES/@antora/site-generator-default/lib/
